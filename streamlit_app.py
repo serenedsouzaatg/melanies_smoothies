ffe5d6faf6aa4c2561543cc951f1f8c4a95a321f
serenedsouzaatg <serene.dsouza@atgeirsolutions.com>
2026-03-31 14:58:17 +0530

    Convert DataFrame to pandas and update multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
-
+pd_df=my_dataframe.to_pandas()
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
