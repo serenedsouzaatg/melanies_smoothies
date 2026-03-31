b671926e5263f7cf73edd6c952f754893e3a6ebe
serenedsouzaatg <serene.dsouza@atgeirsolutions.com>
2026-03-31 13:35:46 +0530

    Update text display with container width option

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅", )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+st.text(smoothiefroot_response.json(), use_container_width=True)
