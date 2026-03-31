f5cd4c865a86ba4d82e13d15ae8eb30382213325
serenedsouzaatg <serene.dsouza@atgeirsolutions.com>
2026-03-31 13:37:51 +0530

    Fix typo in dataframe function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingredient_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
-sf_df = st.datafram(data=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
